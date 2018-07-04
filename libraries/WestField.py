a6308fba6a2690f2c59f48f7fdc34f6edeba9f2f
Miguel A. Dowgopoluk <miggysmalls@hotmail.com>
2018-07-03 21:24:14 -0700

    POC

--- libraries/WestField.py
+++ libraries/WestField.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 import selenium.common
 from robot.utils import asserts
 from robot.api.deco import keyword
@@ -13,8 +14,7 @@ class WestField(object):
 
     def ui_setup(self):
         self.browser = webdriver.Firefox()
-
-        self.browser.implicitly_wait(10)
+        self.browser.implicitly_wait(30)
 
     def ui_teardown(self):
         self.browser.close()
@@ -22,26 +22,27 @@ class WestField(object):
     def go_to_page(self, url):
         self.browser.get(url=url)
 
-    @keyword('Click on ${class_button} to close window')
-    def close_window_by_class(self, class_button):
+    def click_button_by_class(self, class_button):
         try:
-            button = self.browser.find_element_by_class_name(class_button)
-            button.click()
+            self.browser.find_element_by_class_name(class_button).click()
         except selenium.common.exceptions.NoSuchElementException:
             asserts.fail('\nUnable to find class {}!'.format(close))
 
     def enter_take_over(self, class_take_over):
-        take_over = self.browser.find_element_by_class_name(class_take_over)
-        take_over.click()
+        try:
+            take_over = self.browser.find_element_by_class_name(class_take_over)
+            take_over.click()
+        except NoSuchElementException:
+            asserts.fail('****** Expected page element not found!\n'
+                         '****** Unable to locate "{page_element}".\n'.format(page_element=class_take_over))
 
     def click_by_xpath(self, xpath):
-        selection = self.browser.find_element_by_xpath(xpath)
-        selection.click()
+        self.browser.find_element_by_xpath(xpath).click()
 
     def subscribe_form(self, css_selector):
         name_input = self.browser.find_element_by_css_selector(css_selector=css_selector)
 
-    @keyword('XPath ${name}')
-    def get_form_field_by_id(self, name):
-        name_input = self.browser.find_element_by_id(name)
-        name_input.send_keys('Miggy')
+    @keyword('Enter ${text} into ${form_field}')
+    def send_text_to_form(self, form_field, text):
+        self.browser.find_element_by_xpath(text).send_keys(form_field)
+
--- tests/First_UI_Test.robot
+++ tests/First_UI_Test.robot
@@ -9,29 +9,31 @@ Suite Setup         UI Setup
 Suite Teardown      UI Teardown
 
 *** Variables ***
-${site}             https://www.westfield.com/
-${Close_Take_Over}  icon--action.js-close-modal
-${Enter_Take_Over}  modal__newsletter_body
+${site}                     https://www.westfield.com/
+${Close_Take_Over}          icon--action.js-close-modal
+${Enter_Take_Over}          modal__newsletter_body
+${xp_Select_Store}          //main[@id='main']/section/div/div/div[2]/div/div/div[7]/div/a/div/h2
+${css_Select}               \#firstName
+${form_id}                  firstName
+${Fisrt_Name}               Westfield
+${Email}                    westfield-robot3@dispostable.com
+${xp_form_First_Name}       //*[@id="firstName"]
+${xp_form_Email}            //*[@id="email"]
+${Subscribe_Class}                button button--primary u-s-m-none
+${Subscribe_XPath}                /html/body/div[1]/main/section/div[2]/div[1]/form/div/div[2]/div/div/div/button
 
-#${xp_enter_take_over}  //newsletter-modal[@id='newsletter-modal']      /html/body/div[1]/main/div[7]/div[2]/div[2]/a
-${xp_Select_Store}  //main[@id='main']/section/div/div/div[2]/div/div/div[7]/div/a/div/h2
-${css_Select}       \#firstName
-${form_id}          firstName
-${xp_form_id}       //*[@id="firstName"]
-
+${Edit}                 /html/body/div[1]/main/section/div[2]/div[2]/div[2]/div/div/form/div/div/div/div/div/button
 
 *** Test Cases ***
-#Go to site
-#    Go To Page      ${site}
-#
-#Click on an element by class
-#    Click on ${Close_Take_Over} to close window
-#    UI Teardown
-
 Go into take over
 #    UI Setup
-    Go To Page          ${site}
-    enter take over      ${Enter_Take_Over}
-    click_by_xpath      ${xp_Select_Store}
+    Go To Page              ${site}
+    enter take over         ${Enter_Take_Over}
+    click_by_xpath          ${xp_Select_Store}
     sleep  5s
-
+    Enter ${Fisrt_Name} into ${xp_form_First_Name}
+    Enter ${Email} into ${xp_form_Email}
+    sleep  5s
+#    Enter Take Over         ${Subscribe}
+    Click By XPath          ${Subscribe_XPath}
+    sleep  5s
